a7d340345fafc30348b0a4261f224a4d73d8fda9
Ducas Francis <ducas@ducasfrancis.com>
2018-05-17 23:17:58 +1000

    working subscriber

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# azure-service-bus-console-subscriber
-Azure Service Bus subscriber that prints all messages to the console
+# Azure Service Bus Console Subscriber
+Azure Service Bus subscriber that prints all messages to the console.
+
+## Getting Started
+Update the value of *ServiceBus* in appsettings.json with a valid connection string that includes the EntityPath, and the *SubscriptionName* to the name of a subscription you have created. Then just execute `dotnet run -p ServiceBusConsoleSubscriber/ServiceBusConsoleSubscriber.csproj`.
+
+## What it does
+This app simply subscribes to a Service Bus of your choosing and outputs all messages to the console.
+
+## Why?!
+I was looking for a way to verify my publisher was sending correctly formatted messages to a Service Bus, so I needed to pump the messages out to a console...
---
+++ ServiceBusConsoleSubscriber/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ServiceBusConsoleSubscriber
+{
+
+    public class Program
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            Configuration = BuildConfiguration();
+
+            var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) => { e.Cancel = true; cts.Cancel(); };
+
+            Run(cts.Token);
+        }
+
+        private static IConfiguration BuildConfiguration()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return configBuilder.Build();
+        }
+
+        private static void Run(CancellationToken token)
+        {
+            Console.WriteLine("Starting...");
+
+            var count = 0;
+            var receiver = new SubscriptionClient(
+                new ServiceBusConnectionStringBuilder(Configuration.GetConnectionString("ServiceBus")),
+                Configuration["SubscriptionName"]
+                );
+
+            receiver.RegisterMessageHandler(
+                (message, ct) =>
+                {
+                    count++;
+                    var body = Encoding.UTF8.GetString(message.Body);
+                    Console.WriteLine($"{count}: Received {message.MessageId}: {body}");
+                    return Task.CompletedTask;
+                },
+                new MessageHandlerOptions((e) => { Console.WriteLine($"Error: {e.Exception.Message}"); return Task.CompletedTask; }) { AutoComplete = true, MaxConcurrentCalls = 100 });
+
+            token.WaitHandle.WaitOne();
+
+            Console.WriteLine($"Received {count} messages.");
+            Console.WriteLine("Closed.");
+        }
+    }
+}
